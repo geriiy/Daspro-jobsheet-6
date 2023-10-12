0dc9e93721d0e2442898b8019251ff3eaa345f2d
geriiy <gheriyaj@gmail.com>
2023-10-12 14:17:01 +0700

    modifikasi

--- Pemilihan2Percobaan112.java
+++ Pemilihan2Percobaan112.java
@@ -8,15 +8,18 @@ public class Pemilihan2Percobaan112 {
         int tahun = input12.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0)
+            if ((tahun % 100) == 0) {
+            if ((tahun % 400) == 0) {
                 System.out.println("Tahun Kabisat");
-            else
+            
+            } else {
                 System.out.println("Bukan Tahun Kabisat");
-                 
-        } else 
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
-
+        }
         
     }
     
+}
 }
