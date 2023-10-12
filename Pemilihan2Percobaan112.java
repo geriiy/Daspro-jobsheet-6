d3e29ff973f7c3d45e2251c1f69ae5656888652b
geriiy <gheriyaj@gmail.com>
2023-10-12 14:00:16 +0700

    modifikasi1

--- Pemilihan2Percobaan112.java
+++ Pemilihan2Percobaan112.java
@@ -4,12 +4,15 @@ public class Pemilihan2Percobaan112 {
     public static void main(String[] args) {
         Scanner input12 = new Scanner(System.in);
 
-         System.out.print("Masukkan Tahun = ");
+        System.out.print("Masukkan Tahun = ");
         int tahun = input12.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0 || (tahun % 400) == 0)
                 System.out.println("Tahun Kabisat");
+            else
+                System.out.println("Bukan Tahun Kabisat");
+                 
         } else 
             System.out.println("Bukan Tahun Kabisat");
 
